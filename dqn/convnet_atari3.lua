6b4f0ae9b79d731f20a691a03e0b52dacefecde5
Alberto Sabater <asabater94@gmail.com>
2016-03-31 13:11:11 +0200

    Code added. Code updated to ennable screen visualization with a given parameter and loading a trained network

---
+++ dqn/convnet_atari3.lua
@@ -0,0 +1,20 @@
+--[[
+Copyright (c) 2014 Google Inc.
+
+See LICENSE file for full terms of limited license.
+]]
+
+-- DEFINICIÓN DE LAS CAPAS DE LA RED NEURONAL
+
+require 'convnet'
+
+return function(args)
+    args.n_units        = {32, 64, 64}
+    args.filter_size    = {8, 4, 3}
+    args.filter_stride  = {4, 2, 1}
+    args.n_hid          = {512}
+    args.nl             = nn.Rectifier
+
+    return create_network(args)
+end
+
