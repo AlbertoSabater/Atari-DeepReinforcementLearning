2c1d80471285db5d6146084f12e0cbe073b95b58
Alberto Sabater <asabater94@gmail.com>
2016-05-17 14:56:07 +0200

    Fully connected network encapsulated in the main network

--- dqn/convnet.lua
+++ dqn/convnet.lua
@@ -110,22 +110,25 @@ function create_network(args)
 if args.only_conv ~= 1 then
 
     -- reshape all feature planes into a vector per example
-    net:add(nn.Reshape(nel))
+    local netFC = nn.Sequential()
+    netFC:add(nn.Reshape(nel))
 
     -- fully connected layer
-    net:add(nn.Linear(nel, args.n_hid[1]))
-    net:add(args.nl())
+    netFC:add(nn.Linear(nel, args.n_hid[1]))
+    netFC:add(args.nl())
     local last_layer_size = args.n_hid[1]
 
     for i=1,(#args.n_hid-1) do
         -- add Linear layer
         last_layer_size = args.n_hid[i+1]
-        net:add(nn.Linear(args.n_hid[i], last_layer_size))
-        net:add(args.nl())
+        netFC:add(nn.Linear(args.n_hid[i], last_layer_size))
+        netFC:add(args.nl())
     end
 
     -- add the last fully connected layer (to actions)
-    net:add(nn.Linear(last_layer_size, args.n_actions))
+    netFC:add(nn.Linear(last_layer_size, args.n_actions))
+
+    net:add(netFC)
 
 end
 
